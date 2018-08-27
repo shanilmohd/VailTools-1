f5ef2ae70ef4b186fb1220d165f6ebfebfd0a338
Colin Van Oort <cvanoort@uvm.edu>
2018-08-27 15:17:18 -0400

    Added discriminative_loss.py, which attempts to implement the discriminative loss function described in the paper 'Semantic Instance Segmentation with a Discriminative Loss Function'. Updated the __init__.py file in the losses submodule to allow users to import this loss function.

--- vailtools/losses/__init__.py
+++ vailtools/losses/__init__.py
@@ -1,4 +1,5 @@
+from . import numpy_losses
+from .LovaszSoftmax import lovasz_hinge, lovasz_softmax
+from .discriminative_loss import discriminative_loss
 from .losses import iou_loss, class_mean_iou_loss, class_weighted_categorical_crossentropy, \
     error_weighted_categorical_crossentropy, dice_loss, focal_loss, mixed_l1_l2_loss
-from .LovaszSoftmax import lovasz_hinge, lovasz_softmax
-from . import numpy_losses
