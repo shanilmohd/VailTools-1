14ab89e5f529594572462932c53686ec69a1d1f2
Colin Van Oort <cvanoort156@gmail.com>
2020-03-31 16:07:08 -0400

    Reincorporated DropBlock layers via the keras-drop-block package.

--- vailtools/layers/__init__.py
+++ vailtools/layers/__init__.py
@@ -1,6 +1,6 @@
 # TODO Port coordconv and dropblock to TF2
 # from .coordconv import CoordinateChannel1D, CoordinateChannel2D, CoordinateChannel3D
-# from .dropblock import DropBlock1D, DropBlock2D
+from keras_drop_block import DropBlock1D, DropBlock2D
 from .snail import SnailAttentionBlock, SnailDenseBlock, SnailTCBlock
 from .visual_layers import (
     DenseBlock,
