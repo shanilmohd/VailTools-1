9c88bfd610663f1a2a29a83f6e7a258ef8fa8b32
Colin Van Oort <cvanoort156@gmail.com>
2020-04-05 16:01:05 -0400

    Tweaked the import structure for interacting with the Lovasz losses. It seems like the poetry port is working nicely. The only thing that is not working as expected is the editable install, which is a known and long running issue in the python community at the moment.

--- vailtools/losses/__init__.py
+++ vailtools/losses/__init__.py
@@ -1,7 +1,5 @@
 from . import numpy_losses
 
-# TODO: Port LovaszSoftmax to TF2
-# from .LovaszSoftmax import lovasz_hinge, lovasz_softmax
 from .losses import (
     class_mean_iou_loss,
     class_weighted_categorical_crossentropy,
@@ -13,3 +11,4 @@ from .losses import (
     mixed_l1_l2_loss,
     mse,
 )
+from .lovasz import lovasz_hinge, lovasz_softmax
