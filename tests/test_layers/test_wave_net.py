bacb7ed91794cbe520761debbfca3d7561251b7c
Colin Van Oort <cvanoort156@gmail.com>
2020-04-05 13:54:08 -0400

    Updated some tests for the WaveNet block in accordance with the new interface.

--- tests/test_layers/test_wave_net.py
+++ tests/test_layers/test_wave_net.py
@@ -5,7 +5,15 @@ from vailtools.layers import WaveNetBlock
 
 
 def test_wave_net_block():
-    m1 = keras.models.Sequential([WaveNetBlock(filters=32)])
+    input_ = keras.layers.Input((None, 16))
+    skip_out, residual = WaveNetBlock(filters=32)(input_)
+    m1 = keras.models.Model(input_, (skip_out, residual))
     m1.compile(optimizer="adam", loss="mse")
-    m1.fit(np.random.random((32, 16, 16)), np.random.random((32, 16, 16 + 32)))
+    m1.fit(
+        np.random.random((32, 16, 16)),
+        (
+            np.random.random((32, 16, 32)),
+            np.random.random((32, 16, 32))
+        ),
+    )
     m1.summary()
