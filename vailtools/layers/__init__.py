685b2544c15a220fb166220642aa45139f651915
Colin Van Oort <cvanoort156@gmail.com>
2020-04-05 14:09:03 -0400

    Reintroduced coord conv layers. No tests at the moment, will be added ASAP.

--- vailtools/layers/__init__.py
+++ vailtools/layers/__init__.py
@@ -1,6 +1,6 @@
-# TODO Port coordconv to TF2
-# from .coordconv import CoordinateChannel1D, CoordinateChannel2D, CoordinateChannel3D
 from keras_drop_block import DropBlock1D, DropBlock2D
+
+from .coord_conv import CoordinateChannel1D, CoordinateChannel2D, CoordinateChannel3D
 from .snail import SnailAttentionBlock, SnailDenseBlock, SnailTCBlock
 from .visual_layers import (
     DenseBlock,
